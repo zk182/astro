959cd884159b8f6296d644d90ad3fd38aba861ce
Nicolas Battaglia <nicobattmdq@gmail.com>
2024-08-16 13:43:11 +0200

    feat: interactivity

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
+import preact from '@astrojs/preact';
+
 export default defineConfig({
-	site: 'https://jscowboys.netlify.app/'
+	site: 'https://jscowboys.netlify.app/',
+	integrations: [preact()]
 });
